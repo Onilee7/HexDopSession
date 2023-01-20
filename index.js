c561fa4723b7665696ee9e6fca22b6b16ebbc496
Onilee7 <annabardasheva@gmail.com>
2023-01-20 15:49:31 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+const rock = "Камень";
+const paper = "Бумага";
+const scissors = "Ножницы";
+const draw = "Ничья";
+const PlayerWin = "Человек";
+const ComputerWin = "Компьютер просто";
+const mChoice = manInput;
+const pChoice = pcInput; 
+
+function manInput () {
+    let result = null;
+    const manInput = Math.random();
+    if (manInput < 0.34) {
+        result = rock;
+    }
+    else if (manInput < 0.67) {
+        result = scissors;
+    }
+    else result = paper;
+    return result;
+}
+
+
+function pcInput () {
+    let result = null;
+    const manInput = Math.random();
+    if (manInput < 0.34) {
+        result = rock;
+    }
+    else if (manInput < 0.67) {
+        result = scissors;
+    }
+    else result = paper;
+    return result;
+}
+
+function winer (mChoice, pChoice) {
+
+    if (mChoice === pChoice) return draw;
+   
+    else if (pChoice === rock && mChoice === paper ||
+            pChoice === paper && mChoice === scissors ||
+            pChoice === scissors && mChoice === rock){
+
+return PlayerWin;}
+
+     else return ComputerWin;
+    
+    }
+    
+
+
+
+console.log ('Вы показали: ' + manInput());
+console.log ('Компьютер показывает: ' + pcInput());
+console.log('Победил: ' + winer (mChoice, pChoice));
